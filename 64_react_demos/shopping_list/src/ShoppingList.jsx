9d8df63ad74db65690ed40a716c83620b01c4a3c
aditya-arcot <arcot.aditya@utexas.edu>
2023-10-31 00:24:35 -0500

    section 64

---
+++ 64_react_demos/shopping_list/src/ShoppingList.jsx
@@ -0,0 +1,14 @@
+import ShoppingListItem from "./ShoppingListItem"
+
+export default function ShoppingList({ items }) {
+    return (
+        <ul>
+            {items.map(i =>
+                <ShoppingListItem
+                    key={i.id}
+                    {...i}
+                />
+            )}
+        </ul>
+    )
+}
