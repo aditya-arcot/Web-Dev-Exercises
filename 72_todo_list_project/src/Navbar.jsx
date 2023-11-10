9d53b98d8c9a5c5beb5fbb2cd9776ab63d7498ab
aditya-arcot <arcot.aditya@utexas.edu>
2023-11-10 17:10:07 -0600

    section 72

---
+++ 72_todo_list_project/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+
+export default function Navbar() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        React Todos
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    )
+}
---
+++ 72_todo_list_project/src/TodoItem.jsx
@@ -0,0 +1,43 @@
+import { IconButton, ListItem, ListItemButton, ListItemIcon, Checkbox, ListItemText } from "@mui/material"
+import DeleteIcon from '@mui/icons-material/Delete';
+
+export default function TodoItem({ todo, remove, toggle }) {
+    const labelId = `checkbox-list-label-${todo.id}`;
+    const removeTodo = () => {
+        remove(todo.id)
+    }
+    const toggleTodo = () => {
+        toggle(todo.id)
+    }
+    return (
+        <ListItem
+            secondaryAction={
+                <IconButton
+                    edge="end"
+                    aria-label="comments"
+                    onClick={removeTodo}>
+                    <DeleteIcon />
+                </IconButton>
+            }
+            disablePadding
+        >
+            <ListItemButton
+                role={undefined}
+                dense>
+                <ListItemIcon>
+                    <Checkbox
+                        edge="start"
+                        checked={todo.completed}
+                        tabIndex={-1}
+                        disableRipple
+                        inputProps={{ 'aria-labelledby': labelId }}
+                        onChange={toggleTodo}
+                    />
+                </ListItemIcon>
+                <ListItemText
+                    id={labelId}
+                    primary={todo.text} />
+            </ListItemButton>
+        </ListItem>
+    );
+}
